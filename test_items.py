72cb5568697030746341a8b9dce96a48d14f5e4c
Dmitriy Sokolov <falcon22falcon22@gmail.com>
2024-04-13 22:20:23 +0500

    4th commit

--- test_items.py
+++ test_items.py
@@ -9,7 +9,6 @@ import time
 def test_shopping_cart_existence(browser):
     url = r"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(url)
-    time.sleep(5)
     button_element = browser.find_element(By.XPATH, r"//button[contains(@class,'btn-add-to-basket')]")
     try:
         button_element = WebDriverWait(browser, 10).until(EC.element_to_be_clickable(
