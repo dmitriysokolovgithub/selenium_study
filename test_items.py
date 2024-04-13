c596b5c0e809502632f1dbbff4a4796bba6c0e59
Dmitriy Sokolov <falcon22falcon22@gmail.com>
2024-04-13 21:48:23 +0500

    first commit

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium.webdriver.common.by import By
+import time
+
+
+def test_shopping_cart_existence(browser):
+    url = r"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(url)
+    time.sleep(30)
+    button_element = browser.find_element(By.XPATH, r"//button[contains(@class,'btn-add-to-basket')]")
+    assert True
