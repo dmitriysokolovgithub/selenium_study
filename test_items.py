02f4ad357bdb64052f1854505c0b44ca7805c4d5
Dmitriy Sokolov <falcon22falcon22@gmail.com>
2024-04-13 22:05:48 +0500

    second commit

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ import time
 def test_shopping_cart_existence(browser):
     url = r"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(url)
-    time.sleep(30)
+    time.sleep(5)
     button_element = browser.find_element(By.XPATH, r"//button[contains(@class,'btn-add-to-basket')]")
     assert True
